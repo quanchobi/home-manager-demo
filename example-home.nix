f466af5cf3463481b1cbf1e131c82283528a3c25
Jacob Hanks <jacob.hanks@canonical.com>
2026-05-04 12:05:39 -0400

    add: simple nix demo config

---
+++ example-home.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  # 1. Basic user information
+  home.username = "ubuntu";
+  home.homeDirectory = "/home/ubuntu";
+
+  # 2. Package installation
+  home.packages = [
+    pkgs.htop
+    pkgs.git
+    pkgs.nvim
+    pkgs.firefox
+  ];
+
+  # 3. Environment Variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # 4. Program Configurations
+  programs.git = {
+    enable = true;
+    userName = "Nix Demo";
+    userEmail = "nix@demo.com";
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "home-manager switch";
+    };
+  };
+
+  # 5. State Version (Required)
+  home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Home Manager configuration of yourusername";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs (and other inputs)
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux"; # or "aarch64-linux" / "aarch64-darwin"
+      pkgs = nixpkgs.legacyPackages.${system}; # I don't know why it's called legacyPackages
+    in
+    {
+      homeConfigurations."ubuntu" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./example-home.nix ];
+      };
+    };
+}
